e4c5972ba5e8446301ac9d460f5f4e9afac6a7fd
Elijah-Bradley123 <mofilitubefi@gmail.com>
2024-05-09 18:50:25 +0000

    feat: core commit #4

---
+++ contracts/contracts/StepSealToken.sol
@@ -0,0 +1,3 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+contract StepSealToken { string public name = 'StepSealToken'; string public symbol = 'STEP'; uint8 public decimals = 18; }
