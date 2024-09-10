43c0c54fd8a14386ca642dc01ea3ff6da3e9eb70
Letitia-Clarissa <braamtelford@gmail.com>
2024-09-10 22:43:40 +0000

    test: testopt commit #3

--- apps/backend/src/server.js
+++ apps/backend/src/server.js
@@ -4,3 +4,5 @@ app.get('/health', (_, res) => res.json({ ok: true }));
 app.listen(4000, () => console.log('API listening on :4000'));
 
 app.get('/leaderboard', (_, res) => res.json({ users: [] }));
+
+app.get('/metrics', (_, res) => res.json({ uptime: require('./metrics').uptime() }));
